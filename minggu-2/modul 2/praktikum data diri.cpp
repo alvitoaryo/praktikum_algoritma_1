6c855be9c13ecb9cc87c598d50db5986f4c1ed87
alvitoaryo <vitoaryo2@gmail.com>
2020-10-25 21:02:56 +0700

    mengumpulkan tugas minggu 2

---
+++ minggu-2/modul 2/praktikum data diri.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	cout<<" Nama           : Alvito Aryo Prabowo"<<endl;
+	cout<<" Nim            : 123200158"<<endl;
+	cout<<" Kelas          : F IF"<<endl;
+	cout<<" Jenis kelamin  : Laki-Laki"<<endl;
+	cout<<" Alamat         : jln apel,rt03/05,jenang majenang"<<endl;
+	cout<<" e-mail         : vitoaryo2@gmail.com"<<endl;
+	cout<<" No.hp          : 081322561946"<<endl;
+	
+	return 0;
+}
